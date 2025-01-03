38c2c5d76eaa9bc3844c5ecd70f2e593db0336ba
ghina <127872549+barbiemoody@users.noreply.github.com>
2025-01-03 10:35:10 +0700

    Add files via upload

---
+++ config/url.php
@@ -0,0 +1,4 @@
+<?php
+$base_admin = "http://" . $_SERVER['HTTP_HOST'] . "/bk_ghina/pages/admin";
+$base_pasien = "http://" . $_SERVER['HTTP_HOST'] . "/bk_ghina/pages/pasien";
+$base_dokter = "http://" . $_SERVER['HTTP_HOST'] . "/bk_ghina/pages/dokter";
